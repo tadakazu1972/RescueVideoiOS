698c1fbc7ceb5709992c98aeb553434f8f3a09f4
中道忠和 <tadakazu@chuudou-no-mac-mini.local>
2017-10-09 09:57:41 +0900

    Initial Commit

---
+++ RescueVideoiOS/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RescueVideoiOS
+//
+//  Created by 中道 忠和 on 2017/10/09.
+//  Copyright © 2017年 Tadakazu Nakamichi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
